20256cef9cbfa46f18da99e77bab808d36d36f8a
tanglei <lei.tang@icepointcloud.com>
2018-09-07 16:07:33 +0800

    create repository

---
+++ utils/wechat-menu/src/main/java/com/utils/wechat/menu/WechatMenuApplication.java
@@ -0,0 +1,11 @@
+package com.utils.wechat.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class WechatMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatMenuApplication.class, args);
+    }
+}
